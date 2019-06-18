066173e18b0b433fe0fd779ba4d3b58ac737836b
imasif <imasifkhan1010@gmail.com>
2019-06-18 23:35:36 +0600

    some changes on user creation

--- server/boot/scripts.js
+++ server/boot/scripts.js
@@ -1,60 +1,61 @@
 
     // console.log(app.models.user);
     module.exports = function(server) {
-        console.log(server.models.Users)
-        let model = server.models;
-        let Users = model.Users;
-        let Role = model.Role;
-        let RoleMapping = model.RoleMapping;
+        // console.log(server.models.Users)
+    let models = server.models;
+    let Users = models['Users'];
+    let Role = models['Role'];
+    let RoleMapping = models['RoleMapping'];
 
-        //create the admin role
-        Role.create([
+    //create the admin role
+    Role.create([
+        {
+            name: 'admin'
+        },
+        {
+            name: 'employee'
+        }], function(err, role) {
+        if (err) {
+            return console.log(JSON.stringify(err));
+        };
+  
+        console.log('Created role:', role);
+
+
+        Users.create(
             {
-                name: 'admin'
-            },
-            {
-                name: 'employee'
-            }], function(err, role) {
+                role_id: role[0].id,
+                username: "imasifkhan1010@gmail.com",
+                email: 'imasifkhan1010@gmail.com',
+                password: '1q2w3e',
+                first_name:'Asif',
+                last_name: 'Khan',
+                emailVerified: true,
+                realm: "admin",
+                verificationToken: ""
+            }, function(err, user) {
             if (err) {
-                return console.log(JSON.stringify(err));
+                if (err.details) {
+                    return console.log("unknownServerError details: " + JSON.stringify(err.details.codes));
+                }
+                // return console.log(err)
             };
-    
-            console.log('Created role:', role);
 
+            console.log('Created user:', user);
 
-            Users.create(
-                {
-                    role_id: role[0].id,
-                    username: "imasifkhan1010@gmail.com",
-                    email: 'imasifkhan1010@gmail.com',
-                    password: '1q2w3e',
-                    first_name:'Asif',
-                    last_name: 'Khan',
-                    emailVerified: true,
-                    realm: "admin",
-                    verificationToken: ""
-                }, function(err, user) {
-                if (err) {
-                    if (err.details) {
-                        return console.log("unknownServerError details: " + JSON.stringify(err.details.codes));
-                    }
-                    // return console.log(err)
-                };
-
-                console.log('Created user:', user);
-
-                //make asif an admin
-                role[0].principals.create({
-                    principalType: RoleMapping.USER,
-                    principalId: user.id
-                }, function(err, principal) {
-                    if (err) throw err;
-            
-                    console.log('Created principal:', principal);
-                });
-
-
+            //make asif an admin
+            role[0].principals.create({
+                principalType: RoleMapping.USER,
+                principalId: user.id
+            }, function(err, principal) {
+                if (err) throw err;
+        
+                console.log('Created principal:', principal);
             });
-    
+
+
         });
+  
+    });
     }
+    
