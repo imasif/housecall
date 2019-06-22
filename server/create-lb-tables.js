1651b9038762f7452b6e709bfb87c09f539d77cb
alamrezoanul.home <alamrezoanul@gmail.com>
2019-06-23 01:17:34 +0600

    service list and material lists added => should be good now 2

--- server/create-lb-tables.js
+++ server/create-lb-tables.js
@@ -2,8 +2,9 @@ var server = require('./server');
 var ds = server.dataSources.mysqlID;
 // console.log(server);
 // return;
-var lbTables = ['Address','Customer','Job','Phone','Users','AccessToken', 'ACL', 'RoleMapping', 'Role'];
-ds.automigrate(lbTables, function(er) {
+// var lbTables = ['Address','Customer','Job','Phone','Users','AccessToken', 'ACL', 'RoleMapping', 'Role'];
+var lbTables = ['Customer', 'Job', 'Phone', 'Users', 'AccessToken', 'ACL', 'RoleMapping', 'Role'];
+ds.automigrate(lbTables, function (er) {
   if (er) throw er;
   console.log('Loopback tables [' - lbTables - '] created in ', ds.adapter.name);
   ds.disconnect();
