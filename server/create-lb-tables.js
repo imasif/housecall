3525c65216a910c3c49579804daee358fcaa2240
imasif <imasifkhan1010@gmail.com>
2019-06-28 15:19:40 +0600

    schedule model created, some changes in belongs to model relationship, and id uuid for different models

--- server/create-lb-tables.js
+++ server/create-lb-tables.js
@@ -3,7 +3,7 @@ var ds = server.dataSources.mysqlID;
 // console.log(server);
 // return;
 // var lbTables = ['Address','Customer','Job','Phone','Users','AccessToken', 'ACL', 'RoleMapping', 'Role'];
-var lbTables = ['Customer', 'Job', 'Phone', 'Users', 'AccessToken', 'ACL', 'RoleMapping', 'Role'];
+var lbTables = ['Customer', 'Job', 'Users', 'AccessToken', 'ACL', 'RoleMapping', 'Role','Schedule'];
 ds.automigrate(lbTables, function (er) {
   if (er) throw er;
   console.log('Loopback tables [' - lbTables - '] created in ', ds.adapter.name);
